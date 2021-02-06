9b8be84c385d752ccff1d27305ac227618553b62
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-02-06 12:15:44 -0800

    editing routes

--- src/reflections/reflection-router.js
+++ src/reflections/reflection-router.js
@@ -1,62 +1,75 @@
-const express = require('express');
-const ReflectionService = require('./reflection-service');
-const { requireAuth } = require('../middleware/jwt-auth');
+const express = require("express");
+const ReflectionService = require("./reflection-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
 const reflectionRouter = express.Router();
 const jsonParser = express.json();
 
 reflectionRouter
-    .route('/')
-    .get(requireAuth, (req, res, next) => {
-        console.log('called', req.user.id);
-        ReflectionService.getReflections(req.app.get('db'), req.user.id)
-            .then((reflections) => {
-                return res.json(reflections.map(reflection))
-            })
-            .catch(next);
-    })
-    .post(requireAuth, jsonParser, (req, res, next) => {
-        if(!req.body.title){
-            res.status(400).json({ error: 'Title is required'})
-        }
-        const newReflection = req.body;
-        newReflection.userid = req.user.id;
+	.route("/")
+	.get(requireAuth, (req, res, next) => {
+		console.log("called", req.user.id);
+        ReflectionService.getReflections(req.app.get("db"), req.user.id)
+            
+			.then((reflections) => {
+                console.log(reflections)
+				return res.json(reflections);
+			})
+			.catch(next);
+	})
+	.post(requireAuth, jsonParser, (req, res, next) => {
+		if (!req.body.title) {
+			res.status(400).json({ error: "Title is required" });
+		}
+		let newReflection = req.body;
+		newReflection.userid = req.user.id;
 
-        ReflectionService.insertReflection(req.app.get('db'), newReflection)
-            .then((reflection) => {
-                res.status(201).location(`/reflections/${reflection.id}`).json(reflection);
-            })
-            .catch(next);
-    })
+		ReflectionService.insertReflection(req.app.get("db"), newReflection)
+			.then((reflection) => {
+				console.log("return from insert", reflection);
+				res
+					.status(201)
+					.location(`/reflections/${reflection.id}`)
+					.json(reflection);
+			})
+			.catch(next);
+	});
 
 reflectionRouter
-    .route('/:id')
-    .all(requireAuth, (req, res, next) => {
-        cid = parseInt(req.params.id);
-        ReflectionService.getById(req.app.get('db'), cid, req.user.id)
-            .then((reflections) => {
-                if(!reflections){
-                    return res.status(404).json({ error: { message: `reflection doesn't exist` }})
-                }
-                res.reflections = reflections;
-                next();
-            })
-            .catch(next);
-    })
-    .delete(requireAuth, (req, res, next) => {
-        ReflectionService.deleteReflection(req.app.get('db'), cid, req.user.id)
-            .then(() => {
-                res.status(204).end();
-            })
-            .catch(next);
-    })
-    .patch(requireAuth, jsonParser, (req, res, next) => {
-        const updateReflection = req.body;
-        ReflectionService.updateReflection(req.app.get('db'), cid, updateReflection, req.user.id)
-            .then(() => {
-                res.status(200).json((res.updateReflection = updateReflection));
-            })
-            .catch(next);
-    });
+	.route("/:id")
+	.all(requireAuth, (req, res, next) => {
+		cid = parseInt(req.params.id);
+		ReflectionService.getById(req.app.get("db"), cid, req.user.id)
+			.then((reflections) => {
+				if (!reflections) {
+					return res
+						.status(404)
+						.json({ error: { message: `reflection doesn't exist` } });
+				}
+				res.reflections = reflections;
+				next();
+			})
+			.catch(next);
+	})
+	.delete(requireAuth, (req, res, next) => {
+		ReflectionService.deleteReflection(req.app.get("db"), cid, req.user.id)
+			.then(() => {
+				res.status(204).end();
+			})
+			.catch(next);
+	})
+	.patch(requireAuth, jsonParser, (req, res, next) => {
+		const updateReflection = req.body;
+		ReflectionService.updateReflection(
+			req.app.get("db"),
+			cid,
+			updateReflection,
+			req.user.id
+		)
+			.then(() => {
+				res.status(200).json((res.updateReflection = updateReflection));
+			})
+			.catch(next);
+	});
 
 module.exports = reflectionRouter;
--- src/reflections/reflection-service.js
+++ src/reflections/reflection-service.js
@@ -1,37 +1,29 @@
-const ReflectionService = { 
-    getReflections(db, userid){
-        return db
-            .select('*')
-            .from('reflections')
-            .where({ userid })
-    },
-    insertReflection(db, newReflection){
-        return db
-            .insert(newReflection)
-            .into('reflections')
-            .then((rows) => {
-                return rows[0];
-            })
-    },
-    getById(db, id, userid){
-        return db
-            .select('*')
-            .from('reflections')
-            .where({ id: id, userid })
-            .first();
-    },
-    deleteReflection(db, id, userid){
-        return db
-            .from('reflections')
-            .where({ id, userid})
-            .delete();
-    },
-    updateReflection(db, id, updateReflection, userid){
-        return db
-            .from('reflections')
-            .where({ id, userid })
-            .update(updateReflection);
-    }
-}
+const ReflectionService = {
+	getReflections(db, userid) {
+        return db.select("*").from("reflections").where({ userid: userid });
+        
+	},
+	insertReflection(db, newReflection) {
+		return db
+			.insert(newReflection)
+			.into("reflections")
+			.returning("*")
+			.then((rows) => {
+				return rows[0];
+			});
+	},
+	getById(db, id, userid) {
+		return db.select("*").from("reflections").where({ id: id, userid }).first();
+	},
+	deleteReflection(db, id, userid) {
+		return db.from("reflections").where({ id, userid }).delete();
+	},
+	updateReflection(db, id, updateReflection, userid) {
+		return db
+			.from("reflections")
+			.where({ id, userid })
+			.update(updateReflection);
+	},
+};
 
 module.exports = ReflectionService;
