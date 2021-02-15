15e0805064bdbab03a00557fbc681521af352874
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-02-15 15:56:01 -0800

    fixing typo

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL || '',
-  API_BASE_URL: process.env.API_BASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
+	PORT: process.env.PORT || 8000,
+	NODE_ENV: process.env.NODE_ENV || "development",
+	DATABASE_URL: process.env.DATABASE_URL,
+	API_BASE_URL: process.env.API_BASE_URL,
+	JWT_SECRET: process.env.JWT_SECRET,
 };
