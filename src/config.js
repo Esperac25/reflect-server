6a466b723f264370bbe9b5c95a5028680e65d9d0
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-02-15 15:09:32 -0800

    editing config and env credentials

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.DATABASE_URL || '',
   API_BASE_URL: process.env.API_BASE_URL,
-  // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
   JWT_SECRET: process.env.JWT_SECRET,
 };
